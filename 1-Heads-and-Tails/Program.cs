4a184db3927649da951f60c37e5ffe031a3dec20
Velko <v_kapitanov@yahoo.com>
2016-10-08 21:57:16 +0300

    C#

---
+++ 1-Heads-and-Tails/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Heads_and_Tails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> inputs = new List<char>();
+
+            Console.WriteLine("Enterh \"H\" for heads or \"T\" for Tails");
+            string ch = Console.ReadLine().Trim();
+            char[] strToChar = ch.ToCharArray();
+
+            for (int i = 0; i < strToChar.Length; i++)
+            {
+                inputs.Add(strToChar[i]);
+            }
+
+            int counter = 1;
+            string elem = String.Empty;
+            int maxElement = 1;
+
+
+            for (int i = 0; i < inputs.Count - 1; i++)
+            {
+                // H H H T T T
+                if (inputs[i] == inputs[i + 1])
+                {
+                    counter++;
+
+                    if (counter > maxElement)
+                    {
+                        maxElement = counter;
+                        elem = inputs[i].ToString();
+                    }
+                }
+                else
+                {
+                    counter = 1;
+                }
+            }
+
+            if (counter == maxElement)
+            {
+                Console.WriteLine("Draw");
+            }
+            else
+            {
+                Console.WriteLine("Winner element is {0}", elem);
+            }
+        }
+    }
+}
---
+++ 2-Lakes/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Lakes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int levelDown = 0;
+            int levelUp = 0;
+            int sum = 0;
+
+            Console.WriteLine("Please input \"d\" for down level");
+            Console.WriteLine("Please input \"u\" for up level");
+            Console.WriteLine("Please input \"h\" for horizontal level");
+
+            //read from console
+            string inputChar = Console.ReadLine();
+
+            // walking throughout the levels
+            for (int i = 0; i < inputChar.Count(); i++)
+            {
+                if (inputChar[i] == 'd')
+                {
+                    if (levelUp > 1)
+                    {
+                        levelUp--;
+                    }
+                    else
+                    {
+                        //count and sum down-levels
+                        levelDown++;
+                        sum += 500 + (levelDown - 1) * 1000;
+                    }
+                }
+                else if (inputChar[i] == 'u')
+                {
+                    if (levelDown > 0)
+                    {
+                        //count going up-levels and remove a level from "down"
+                        levelDown--;
+                        sum += 500 + (levelDown * 1000);
+                        levelUp++;
+                    }
+                }
+                // horizontal sum level
+                else
+                {
+                    if (inputChar[i] == 'h')
+                    {
+                        sum += 1000 * levelDown;
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
